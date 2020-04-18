cb2578bc66727e938466e030c8eafe7b6598f87d
zhaoliangwan <zhaoliangwan520@163.com>
2020-04-18 20:55:56 +0800

    优化轮播图

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const glide = new Glide(".glide");
+const captionsEL = document.querySelector(".slide-caption");
+
+glide.on("mount.after", "run.after"),() =>{
+    const caption = captionsEL[glide.index];
+    anime({
+        targets: caption.childern,
+        opacity: [0,1],
+        duration: 400,
+        easing: "linear",
+        delay: anime.stagger(400, {start: 300}),
+
+    })
+}
 
 glide.mount();
